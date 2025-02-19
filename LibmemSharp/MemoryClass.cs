f99ea97fc2d34d88bf2d98df77ba8c6b968c5ce2
Rikko Matsumato <kuttimsx@hotmail.com>
2025-02-20 01:55:42 +0200

    Add files via upload

---
+++ LibmemSharp/MemoryClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibmemSharp
+{
+    public class MemoryClass
+    {
+        private static string l_mem = @"libmem.dll";
+        /* 
+          Coming Soon!!!
+        */
+    }
+}
